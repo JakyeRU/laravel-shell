6f36badc6ad10ecf7dd4dbb1b5576f3d79576078
Jakye <jakyeru@gmail.com>
2023-02-04 20:09:28 +0200

    Updated laravel-shell.php

--- src/config/laravel-shell.php
+++ src/config/laravel-shell.php
@@ -4,11 +4,42 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Title
+    | Laravel Shell Enabled
     |--------------------------------------------------------------------------
     |
-    | Description
+    | This option controls whether the Laravel Shell is enabled or not.
     |
     */
+
+    'enabled' => env('LARAVEL_SHELL_ENABLED', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Route Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the route that will be used to access Laravel
+    | Shell.
+    |
+    */
+
+    'route' => [
+        'prefix' => 'laravel-shell',
+        'middleware' => ['web'],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Commands
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the custom commands that will be available in
+    | Laravel Shell.
+    |
+    */
+
+    'commands' => [
+        // \App\Commands\CustomCommand::class,
+    ],
 
 ];
