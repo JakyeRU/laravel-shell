af15a2f32c84381ee5fb23b34670a87118aa3d8e
Jakye <jakyeru@gmail.com>
2023-02-04 20:35:01 +0200

    Added web.php

---
+++ src/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Laravel Shell Routes
+|--------------------------------------------------------------------------
+| These are the routes used by Laravel Shell.
+|
+*/
+
+Route::group(config('laravel-shell.route'), function () {
+
+});
