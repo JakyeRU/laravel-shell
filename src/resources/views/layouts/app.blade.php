2cbbed246d0b2baf32d64593cbe58e9f546a18d1
Jakye <jakyeru@gmail.com>
2023-02-05 11:28:43 +0200

    Added layout.

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Shell</title>
+
+    <!-- Styles -->
+    <link href="{{ route('laravel-shell.assets.serve', 'xterm.css') }}" rel="stylesheet">
+    @livewireStyles
+</head>
+<body style="background-color: rgb(40, 42, 54);">
+    {{ $slot }}
+    <!-- Scripts -->
+    @livewireScripts
+    <script src="{{ route('laravel-shell.assets.serve', 'xterm.js') }}"></script>
+</body>
+</html>
