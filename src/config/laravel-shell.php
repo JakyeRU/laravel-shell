07439a4235ba829c8da056a51fcdb611e6767871
Jakye <jakyeru@gmail.com>
2023-02-09 15:33:13 +0200

    Added terminal customization.

--- src/config/laravel-shell.php
+++ src/config/laravel-shell.php
@@ -28,4 +28,45 @@ return [
         'middleware' => ['web'],
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Terminal Customization
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure how the terminal will look.
+    |
+    */
+
+    'terminal' => [
+        'colors' => [
+            'foreground' => '#eff0eb',
+            'background' => '#282a36',
+            'cursorAccent' => '#282a36',
+            'selection' => '#97979b33',
+            'black' => '#282a36',
+            'brightBlack' => '#686868',
+            'red' => '#890301',
+            'brightRed' => '#ff5c57',
+            'green' => '#5af78e',
+            'brightGreen' => '#5af78e',
+            'yellow' => '#f3f99d',
+            'brightYellow' => '#f3f99d',
+            'blue' => '#57c7ff',
+            'brightBlue' => '#57c7ff',
+            'magenta' => '#ff6ac1',
+            'brightMagenta' => '#ff6ac1',
+            'cyan' => '#9aedfe',
+            'brightCyan' => '#9aedfe',
+            'white' => '#f1f1f0',
+            'brightWhite' => '#eff0eb',
+        ],
+        'startup' => [
+            'Welcome to \x1b[34;1mLaravel Shell\x1b[0m \x1b[93;1mv' . \Jakyeru\LaravelShell\LaravelShellServiceProvider::VERSION . '\x1b[0m!',
+            'Running Laravel \x1b[93;1mv' . Illuminate\Foundation\Application::VERSION . '\x1b[0m (PHP \x1b[93;1mv' . PHP_VERSION . '\x1b[0m)\r\n',
+        ],
+        'showInteractiveWarning' => true,
+        'cursorBlink' => true,
+        'prompt' => '$ ',
+    ],
+
 ];
